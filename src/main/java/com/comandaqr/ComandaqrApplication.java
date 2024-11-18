e9e2d3f1b118d1b8da69644c8ecbbea08fe30881
joao-erik2077 <joaoerik2074@gmail.com>
2024-11-18 20:48:03 -0300

    Initial commit

---
+++ src/main/java/com/comandaqr/ComandaqrApplication.java
@@ -0,0 +1,13 @@
+package com.comandaqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandaqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandaqrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comandaqr/ComandaqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comandaqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandaqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
