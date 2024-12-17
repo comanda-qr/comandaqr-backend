194ea0388c375d8568ebe2c98cee3e738ea9567f
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-12-17 14:42:27 -0300

    (feat): create repositories for models

---
+++ src/main/java/com/comandaqr/repositories/BusinessRepository.java
@@ -0,0 +1,11 @@
+package com.comandaqr.repositories;
+
+import com.comandaqr.models.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BusinessRepository extends JpaRepository<Business, UUID> {
+}
